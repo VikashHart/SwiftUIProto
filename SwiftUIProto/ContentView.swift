c0fda132f425666e7eda2854350142d0c8538190
Vikash Hart <vikashhart@gmail.com>
2021-10-22 23:27:39 -0400

    score formula added

--- SwiftUIProto/ContentView.swift
+++ SwiftUIProto/ContentView.swift
@@ -8,6 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    let rTarget = Double.random(in: 0..<1)
+    let gTarget = Double.random(in: 0..<1)
+    let bTarget = Double.random(in: 0..<1)
+    @State var rGuess: Double
+    @State var gGuess: Double
+    @State var bGuess: Double
+    @State var showAlert = false
+
+    func computeScore() -> Int {
+      let rDiff = rGuess - rTarget
+      let gDiff = gGuess - gTarget
+      let bDiff = bGuess - bTarget
+      let diff = sqrt(rDiff * rDiff + gDiff * gDiff + bDiff * bDiff)
+      return Int((1.0 - diff) * 100.0 + 0.5)
+    }
     var body: some View {
         Text("Hello, world!")
             .padding()
@@ -16,6 +31,5 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-    }
+        ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5)    }
 }
